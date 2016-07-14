b3cdc74e5ccc6202ae81a5ae2386cd54481a37c1
lyx5710317 <315851414@qq.com>
2016-07-14 11:18:49 +0800

    科云电商var1.0

---
+++ catalog/language/zh-CN/account/address.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']     = '公司信息';
+
+// Text
+$_['text_account']      = '我的账户';
+$_['text_address_book'] = '公司信息';
+$_['text_edit_address'] = '您的公司信息';
+$_['text_add']             = '您的信息已经成功填写！';
+$_['text_edit']            = '您的信息已经成功修改！';
+$_['text_delete']          = '您的信息已删除';
+$_['text_empty']           = '您还没有信息。';
+
+// Entry
+$_['entry_firstname']   = '您的姓名';
+$_['entry_company']     = '公司名称';
+$_['entry_address_1']      = '地址';
+$_['entry_postcode']       = '行业';
+$_['entry_city']           = '区/县';
+$_['entry_country']        = '省/直辖市';
+$_['entry_zone']        = '城市';
+$_['entry_default']     = '默认地址';
+
+// Error
+$_['error_delete']      = '警告： 您必须填写一个公司信息！';
+$_['error_default']     = '警告： 您不能注销您的默认信息！';
+$_['error_firstname']   = '姓氏必须在1到32字符之间！';
+$_['error_vat']         = 'VAT 号码不存在！';
+$_['error_address_1']   = '地址必须在2到128字符之间！';
+$_['error_postcode']    = '行业必须在2到10字符之间！';
+$_['error_city']        = '区/县名必须在2到128字符之间！';
+$_['error_country']     = '请选择一个省/直辖市！';
+$_['error_zone']        = '请选择一个城市！';
+$_['error_custom_field']   = '%s 必须填写！';
+$_['error_contactn']      = '您已经成功联系我们公司，我们会尽快和您取得联系!';
+$_['error_contacty']      = '您已经联系过我们公司，请勿重复联系，我们会尽快给您电话!';
